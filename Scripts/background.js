9055c4e8c05c62c57a01be214a78dd6d59751bd7
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2018-09-01 23:43:05 -0400

    I don't understand authentication with Youtube

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,1 +1,14 @@
-/* console.log("Background script activated"); */
+console.log("Background script activated");
+
+/* chrome.identity.getAuthToken({interactive: true}, function(token)
+{
+    console.log(token);
+}); */
+
+/* window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+      });
+    });
+  }; */
