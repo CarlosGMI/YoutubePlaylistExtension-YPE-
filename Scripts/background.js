5365941c33b1eb6dd9210ff755cea0cb654ce191
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2018-09-09 23:14:14 -0400

    Authentication done. Figuring out a way to store securely the access token

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,37 +1,39 @@
 console.log("Background script activated");
 
-/* chrome.identity.getAuthToken({interactive: true}, function(token)
+let urlOauth2 = "https://accounts.google.com/o/oauth2/auth?";
+let idCliente = "1068019676011-3q6tfmrv9oiv5vn3nrdie0bb82n4plo3.apps.googleusercontent.com";//"1068019676011-i8foh6s791hgvalphsesukblafn6src8.apps.googleusercontent.com";
+let redirect_uri = "http%3A%2F%2Flocalhost%2Foauth2callback";//chrome.identity.getRedirectURL("oauth2"); //'https://'+chrome.runtime.id+'.chromiumapp.org'; //"http%3A%2F%2Flocalhost%2Foauth2callback";
+let scope = "https://www.googleapis.com/auth/youtube";
+let response_type = "token";
+let authUrl = urlOauth2+'&client_id='+idCliente+'&redirect_uri='+redirect_uri+'&response_type='+response_type+'&scope='+scope;
+
+/* chrome.identity.launchWebAuthFlow({'url': authUrl, 'interactive': true}, function(redUrl)
 {
+  console.log("Entramos acá");
+  var q = redUrl.substr(redUrl.indexOf('#')+1);
+  var parts = q.split('&');
+  for (var i = 0; i < parts.length; i++) {
+    var kv = parts[i].split('=');
+    if (kv[0] == 'access_token') {
+      token = kv[1];
+      console.log('token is', token);
+    }
+  }
+  console.log(redUrl);
+}) */
+
+/* $.get(authUrl,obtenerToken);
+
+function obtenerToken(response)
+{
+  chrome.tabs.update({url: authUrl});
+} */
+
+/* chrome.identity.getAuthToken({'interactive': true}, function(token)
+{
+  if (chrome.runtime.lastError) 
+  {
+    alert(chrome.runtime.lastError.message);
+  }
     console.log(token);
 }); */
-
-/* window.onload = function() {
-    document.querySelector('button').addEventListener('click', function() {
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        console.log(token);
-      });
-    });
-  }; */
-
-  /* window.onload = function() {
-    document.querySelector('button').addEventListener('click', function() {
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        let init = {
-          method: 'GET',
-          async: true,
-          headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json'
-          },
-          'contentType': 'json'
-        };
-        fetch(
-            'https://people.googleapis.com/v1/contactGroups/all?maxMembers=20&key=<API_Key_Here>',
-            init)
-            .then((response) => response.json())
-            .then(function(data) {
-              console.log(data)
-            });
-      });
-    });
-  }; */
--- Scripts/popup.js
+++ Scripts/popup.js
@@ -2,14 +2,80 @@ $(document).ready(function(){
     $('select').formSelect();
 });
 
-console.log("Popup activated");
-let url = "https://accounts.google.com/o/oauth2/auth?";
-let idCliente = "1068019676011-i8foh6s791hgvalphsesukblafn6src8.apps.googleusercontent.com";
-let redirect_uri = "https://127.0.0.1";
+let urlOauth2 = "https://accounts.google.com/o/oauth2/auth?";
+let idCliente = "1068019676011-ioguch2k68ppfhhjk4c34m0gm3falu6m.apps.googleusercontent.com";//"1068019676011-i8foh6s791hgvalphsesukblafn6src8.apps.googleusercontent.com";
+let redirect_uri = chrome.identity.getRedirectURL("oauth2");//chrome.identity.getRedirectURL();//chrome.identity.getRedirectURL("oauth2");//"http%3A%2F%2Flocalhost%2Foauth2callback";"http://localhost/oauth2callback";
 let scope = "https://www.googleapis.com/auth/youtube";
 let response_type = "token";
-$.get(url+'&client_id='+idCliente+'&redirect_uri='+redirect_uri+'&response_type='+response_type+'&scope='+scope, function(response, data, some)
+let authUrl = urlOauth2+'&client_id='+idCliente+'&redirect_uri='+redirect_uri+'&response_type='+response_type+'&scope='+scope;
+
+//ESTO FUNCIONA PERFECTO EN OPERA
+chrome.identity.launchWebAuthFlow({'url': authUrl, 'interactive': true}, function(redirectUrl)
 {
-  console.log(url+'&client_id='+idCliente+'&redirect_uri='+redirect_uri+'&response_type='+response_type+'&scope='+scope)
-  console.log("Somos "+JSON.stringify(some));
+    if (redirectUrl) {
+        console.log(redirectUrl);
+        var token = getTokenFromURL(redirectUrl);
+        console.log("El maravilloso token es: "+token);
+        validateToken(token);
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/channels?part=id&mine=true",
+            beforeSend: function(xhr)
+            {
+                xhr.setRequestHeader("Authorization", "Bearer "+token);
+            },
+            success: function(data)
+            {
+                console.log(JSON.stringify(data));
+            }
+        });
+    }
+    else
+    {
+        if (chrome.runtime.lastError)
+        {
+            alert("Ocurrió el siguiente error al autenticar: "+chrome.runtime.lastError.message);
+        }
+        else
+        {
+            alert("Ocurrió un error al autenticar");
+        } 
+    }
 });
+
+function getTokenFromURL(url) 
+{
+    var q = url.substr(url.indexOf('#') + 1);
+    var parts = q.split('&');
+    for (var i = 0; i < parts.length; i++) 
+    {
+        var kv = parts[i].split('=');
+        if (kv[0] == 'access_token') 
+        {
+            token = kv[1];
+            return token;
+        }
+    }
+}
+
+function validateToken(token) {
+    $.post("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+token, function(data, status)
+    {
+        if(status === "success")
+        {
+            console.log(JSON.stringify(data));
+        }
+        else
+        {
+            alert("Token inválido!");
+        }
+    });
+}
+
+//ESTO FUNCIONA PERFECTO EN CHROME
+// let idCliente = "1068019676011-kt5qv58g0q5krvdeek58q94r9ojrcv7o.apps.googleusercontent.com";
+/* chrome.identity.getAuthToken({interactive: true}, function(token)
+{
+    //alert(chrome.runtime.lastError.message);
+    alert("Sali por aqui: "+token);
+    console.log(token);
+}); */
