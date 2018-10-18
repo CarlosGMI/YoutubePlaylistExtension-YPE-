69cb35685eeceb77df1da1891985b8c2e9e78a4a
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2018-10-17 23:02:24 -0400

    Channel and playlists in JSON format

--- Scripts/popup.js
+++ Scripts/popup.js
@@ -17,7 +17,7 @@ OperaAuthentication();
  * Función que gestiona la autenticación de Opera. Si no hay cookie, inicia la autenticación en Youtube por OAauth2 
  * dado el authUrl. Si existe dicha cookie debe llenar el popup con la información de los playlists.
  */
-function OperaAuthentication()
+async function OperaAuthentication()
 {
     if(getCookie("YPE") == false)
     {
@@ -29,9 +29,9 @@ function OperaAuthentication()
     }
     else
     {
-        alert("Ya estoy logeado en YPE :D");
-        let cookie = getCookie("YPE");
-        alert(cookie);
+        console.log("Ya estoy logeado en YPE :D");
+        let tokenInCookie = getCookie("YPE");
+        let idCanal = await obtenerInfo(tokenInCookie);
     }
 }
 
@@ -51,9 +51,9 @@ function OperaRedirectURL(redirectUrl)
     {
         console.log("Redirect URL from Chrome.identity: " + redirectUrl);
         var token = getTokenFromURL(redirectUrl);
-        console.log("El maravilloso token es: " + token);
+        //console.log("El maravilloso token es: " + token);
         ValidateTokenRequest(token)
-            .then((json) => setCookie("YPE", token, "15"))
+            .then((json) => /* getRefreshToken(token) */setCookie("YPE", token, "600"))
             .catch((e) => alert(e));
     }
     else
@@ -78,13 +78,26 @@ async function ValidateTokenRequest(token)
         throw Error("Ha ocurrido un error validando tus credenciales de acceso");
 }
 
+async function getRefreshToken(token)
+{
+    let checkToken = await fetch("https://accounts.google.com/o/oauth2/token?&client_id="+idCliente+"&client_secret=AIzaSyAL5AUBoY46yjT66rnIX3E8e8JlHMr0ies&refresh_token="+token+"&grant_type=refresh_token");
+    console.log(checkToken);
+}
+
 function setCookie(name, value, duration)
 {
+    //chrome.cookies.set({"url": "https://*/*", "name": name, "value": value, "expirationDate": duration});
     document.cookie = name+"="+value+"; max-age="+duration;
 }
 
 function getCookie(name)
 {
+    //chrome.cookies.get({"url": "https://*/*", "name": name}, function(cookie){
+      //  if(cookie)
+        //    return cookie;
+        //else
+        //    return false;
+    //})
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) 
@@ -116,7 +129,7 @@ function getTokenFromURL(url)
 }
 
 //SOLICITUD API PARA OBTENER LOS CANALES CORRESPONDIENTES AL ACCESS TOKEN ESPECIFICADO
-/* function obtenerCanal() 
+function obtenerInfo(token) 
 {
     $.ajax({
             url: "https://www.googleapis.com/youtube/v3/channels?part=id&mine=true",
@@ -126,10 +139,33 @@ function getTokenFromURL(url)
             },
             success: function(data)
             {
-                console.log(JSON.stringify(data));
+                console.log("La respuesta de los canales es: "+JSON.stringify(data));
+                obtenerPlaylists(token, data.items[0].id);
             }
         });
-} */
+}
+
+function obtenerPlaylists(token, idCanal)
+{
+    $.ajax({
+        //url: "https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=20&id=PL-JD1GaONZFQ8ZSH1yNZVuv27p8EX5mGV",
+        url: "https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true&maxResults=20",
+        beforeSend: function(xhr)
+        {
+            xhr.setRequestHeader("Authorization", "OAuth "+token);
+        },
+        success: function(data)
+        {
+            //actualizarPlaylistList(data.items[0].id, data.items[0].snippet.title);
+            console.log(JSON.stringify(data.items));
+        }
+    });
+}
+
+function actualizarPlaylistList(idPlaylist, nombrePlaylist)
+{
+    console.log("Entro por acá con ID: "+idPlaylist+" --- y nombre: "+nombrePlaylist);
+}
 
 //Autenticación para Chrome
 // let idCliente = "1068019676011-kt5qv58g0q5krvdeek58q94r9ojrcv7o.apps.googleusercontent.com";
